9d292957255d3fafa8b5da911aac9a9c92bb818d
Freddy Coaricona <fcoaricona@outlook.com>
2022-11-08 00:16:11 -0400

    ejercicio - Sintaxis, variables y palabras reservadas

---
+++ 02-Sintaxis/ejercicio-sintaxis.js
@@ -0,0 +1,13 @@
+const datos = [
+    "Freddy",
+    23,
+    true,
+    new Date(1999, 2, 22),
+    libro = {
+        titulo: "Historia de dos ciudades",
+        autor: "Charles Dickens",
+        fecha: "1859",
+        url: "https://www.amazon.es/Historia-ciudades-Minus-Charles-Dickens/dp/8484287289/ref=sr_1_1?keywords=historia+de+dos+ciudades+charles+dickens&qid=1667880711&qu=eyJxc2MiOiIzLjYxIiwicXNhIjoiMy4yMyIsInFzcCI6IjMuNDIifQ%3D%3D&s=books&sprefix=historia+de+dos+ciudades+cha%2Cstripbooks%2C347&sr=1-1",
+    }
+];
+console.log(datos);
