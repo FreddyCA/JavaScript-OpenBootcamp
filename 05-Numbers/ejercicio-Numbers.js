b1bbf45478073e4f037431b6e157fab37670eb35
Freddy Coaricona <fcoaricona@outlook.com>
2022-11-08 23:34:30 -0400

    ejercicio - Numbers: redondeo, evaluar MAX_VALUE

---
+++ 05-Numbers/ejercicio-Numbers.js
@@ -0,0 +1,12 @@
+let altura_cm = 168;
+let altura_m = 1.68;
+let peso_kg = 70.5;
+
+let redonde_sup = Math.ceil(altura_m);
+console.log(redonde_sup);
+
+let redonde_inf = Math.floor(peso_kg);
+console.log(redonde_inf);
+
+let igual = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
+console.log(igual);
