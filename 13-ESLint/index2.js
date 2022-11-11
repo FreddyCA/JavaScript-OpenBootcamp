8883e6f55be63dfa63a188315eecaef7490203ec
Freddy Coaricona <fcoaricona@outlook.com>
2022-11-11 19:46:56 -0400

    ejercicio - ESLint: install - ecmaVersion - lint-fix

---
+++ 13-ESLint/index2.js
@@ -0,0 +1,32 @@
+let nombre = "Freddy"
+let apellido = "Coaricona"
+let estudiante = nombre.concat(" ", apellido)
+console.log(estudiante)
+
+// datos estudiante en mayúscula
+let estudianteMayus = estudiante.toUpperCase()
+console.log(estudianteMayus)
+
+// datos estudiante en minúscula 
+let estudianteMinus = estudiante.toLowerCase()
+console.log(estudianteMinus)
+
+// número de letras de: estudiante
+let estudianteTamanho = estudiante.length
+console.log(estudianteTamanho)
+
+// primera letra del: nombre
+let nombrePrimerLetra = nombre[0]
+console.log(nombrePrimerLetra)
+
+// última letra del: apellido
+let apellidoUltimaLetra = apellido[apellido.length-1]
+console.log(apellidoUltimaLetra)
+
+// eliminando espacio en: estudiante
+let estudianteSinEspacio = estudiante.replace(" ", "")
+console.log(estudianteSinEspacio)
+
+// verificando si existe el nombre en estudiante
+let verificando = estudiante.includes(nombre)
+console.log(verificando)
